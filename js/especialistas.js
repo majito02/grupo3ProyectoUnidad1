7be1fbd93afa17099d5ea036db0ad938ecf08254
Majito♥ <89418133+majito02@users.noreply.github.com>
2022-06-05 21:15:04 -0500

    MODIFICACIÓN DE LOS ESPECIALISTAS DE CADA RAMA

--- js/especialistas.js
+++ js/especialistas.js
@@ -21,31 +21,31 @@ const especialistas = [{"img":"img/Trabajos/1.jpg","nombre":"Alejandra Torres","
       {"img":"img/Trabajos/7.jpg","nombre":"Melany Cueva","categoria":"Psiquiatría"}];
       
 const farmacoterapia = [{"img":"img/Trabajos/1.jpg","nombre":"Alejandra Torres","categoria":"Seguridad del personal sanitario"},
-      {"img":"img/Trabajos/2.png","nombre":"Dayana Navarro","categoria":"Angiología"},
-      {"img":"img/Trabajos/3.jpg","nombre":"Leonardo Cruz","categoria":"Genética"},
-      {"img":"img/Trabajos/4.jpg","nombre":"Carlos Curiel","categoria":"Hematología"},
-      {"img":"img/Trabajos/5.png","nombre":"Steven Varela","categoria":"Nefrología"},
-      {"img":"img/Trabajos/8.jpg","nombre":"Milena Viveres","categoria":"Neurología"},
-      {"img":"img/Trabajos/6.jpg","nombre":"Kevin Aranda","categoria":"Pediatría"},
-      {"img":"img/Trabajos/7.jpg","nombre":"Melany Cueva","categoria":"Psiquiatría"}];
+      {"img":"img/Trabajos/2.png","nombre":"Maria Párraga","categoria":"Angiología"},
+      {"img":"img/Trabajos/3.jpg","nombre":"Vinicio Borja","categoria":"Genética"},
+      {"img":"img/Trabajos/4.jpg","nombre":"Carlos Enriquez","categoria":"Hematología"},
+      {"img":"img/Trabajos/5.png","nombre":"Eduardo Valencia","categoria":"Nefrología"},
+      {"img":"img/Trabajos/8.jpg","nombre":"Kelly Gomez","categoria":"Neurología"},
+      {"img":"img/Trabajos/6.jpg","nombre":"Elian Gualpa","categoria":"Pediatría"},
+      {"img":"img/Trabajos/7.jpg","nombre":"Carmen Ruiz","categoria":"Psiquiatría"}];
 
 const psicologos = [{"img":"img/Trabajos/1.jpg","nombre":"Alejandra Torres","categoria":"Seguridad del personal sanitario"},
-      {"img":"img/Trabajos/2.png","nombre":"Dayana Navarro","categoria":"Angiología"},
-      {"img":"img/Trabajos/3.jpg","nombre":"Leonardo Cruz","categoria":"Genética"},
-      {"img":"img/Trabajos/4.jpg","nombre":"Carlos Curiel","categoria":"Hematología"},
-      {"img":"img/Trabajos/5.png","nombre":"Steven Varela","categoria":"Nefrología"},
-      {"img":"img/Trabajos/8.jpg","nombre":"Milena Viveres","categoria":"Neurología"},
-      {"img":"img/Trabajos/6.jpg","nombre":"Kevin Aranda","categoria":"Pediatría"},
-      {"img":"img/Trabajos/7.jpg","nombre":"Melany Cueva","categoria":"Psiquiatría"}];
+      {"img":"img/Trabajos/2.png","nombre":"Luisa Palma","categoria":"Angiología"},
+      {"img":"img/Trabajos/3.jpg","nombre":"Eduardo Moreira","categoria":"Genética"},
+      {"img":"img/Trabajos/4.jpg","nombre":"Pablo Moreira","categoria":"Hematología"},
+      {"img":"img/Trabajos/5.png","nombre":"Edison Romero","categoria":"Nefrología"},
+      {"img":"img/Trabajos/8.jpg","nombre":"Marilyn Santos","categoria":"Neurología"},
+      {"img":"img/Trabajos/6.jpg","nombre":"Luis Párraga","categoria":"Pediatría"},
+      {"img":"img/Trabajos/7.jpg","nombre":"Camila Párraga","categoria":"Psiquiatría"}];
       
 const fisioterapeutas = [{"img":"img/Trabajos/1.jpg","nombre":"Alejandra Torres","categoria":"Seguridad del personal sanitario"},
-      {"img":"img/Trabajos/2.png","nombre":"Dayana Navarro","categoria":"Angiología"},
-      {"img":"img/Trabajos/3.jpg","nombre":"Leonardo Cruz","categoria":"Genética"},
-      {"img":"img/Trabajos/4.jpg","nombre":"Carlos Curiel","categoria":"Hematología"},
-      {"img":"img/Trabajos/5.png","nombre":"Steven Varela","categoria":"Nefrología"},
-      {"img":"img/Trabajos/8.jpg","nombre":"Milena Viveres","categoria":"Neurología"},
-      {"img":"img/Trabajos/6.jpg","nombre":"Kevin Aranda","categoria":"Pediatría"},
-      {"img":"img/Trabajos/7.jpg","nombre":"Melany Cueva","categoria":"Psiquiatría"}];
+      {"img":"img/Trabajos/2.png","nombre":"Maribel Moreira","categoria":"Angiología"},
+      {"img":"img/Trabajos/3.jpg","nombre":"Fernando Burgos","categoria":"Genética"},
+      {"img":"img/Trabajos/4.jpg","nombre":"Kevin Paute","categoria":"Hematología"},
+      {"img":"img/Trabajos/5.png","nombre":"Luis Gomez","categoria":"Nefrología"},
+      {"img":"img/Trabajos/8.jpg","nombre":"Jonsu Burgos","categoria":"Neurología"},
+      {"img":"img/Trabajos/6.jpg","nombre":"Katerine Lopez","categoria":"Pediatría"},
+      {"img":"img/Trabajos/7.jpg","nombre":"Selena Rivas","categoria":"Psiquiatría"}];
 
 //formacion de Array para automatizar
 opciones_botones = [especialistas,farmacoterapia,psicologos,fisioterapeutas];
